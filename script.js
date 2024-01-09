54d527ba4eaadfab4bdaa4647c48a739cd813509
EnzoPierucci <enzopierucci@gmail.com>
2024-01-09 10:55:39 +0100

    Milestone 2

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const form = document.getElementById('form');
 
-form.addEventListener('submit', Ticket)
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-function Ticket (form) {
-
-    form.preventDefault();
-
+    let name = document.getElementById("nomeInput").value;
     let km = document.getElementById("kmInput").value;
     let etaCliente = document.getElementById('ageInput').value;
-    
-    
+
     let prezzoTotale = km * 0.21;
     if (etaCliente < 18) {
         prezzoTotale *= 0.8;
@@ -18,8 +15,37 @@ function Ticket (form) {
     }
     prezzoTotale = prezzoTotale.toFixed(2);
     console.log(`Il prezzo totale del viaggio è: ${prezzoTotale} €`);
-    document.writeln(prezzoTotale)
+
+    addTableRow(name, 1, km, etaCliente, prezzoTotale);
+});
+
+function addTableRow(name, num, km, age, price) {
+    const tbo = document.getElementById('tbo');
+
+    const tr = document.createElement('tr');
+    tbo.appendChild(tr);
+
+    const thName = document.createElement('th');
+    tr.appendChild(thName);
+    thName.innerHTML = name;
+
+    const thNum = document.createElement('th');
+    tr.appendChild(thNum);
+    thNum.innerHTML = num;
+
+    const kmtd = document.createElement('th');
+    tr.appendChild(kmtd);
+    kmtd.innerHTML = km;
+    
+    const agetd = document.createElement('th');
+    tr.appendChild(agetd);
+    agetd.innerHTML = age;
+
+    const pricetd = document.createElement('th');
+    tr.appendChild(pricetd);
+    pricetd.innerHTML = price + ' €';
 }
+
 
 
 
